082a9a246b0a44429e50c1343b92272d2ebb6bcd
Harshal Patel <patelharshal101@gmail.com>
2023-04-04 23:42:47 -0600

    Changing folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import LandingNav from "./components/LandingNav"
-import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 export default function App() {
   return (
     <div className="w-screen h-screen bg-black">
-      <BrowserRouter>
-        <LandingNav />
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LandingNav from '../components/LandingNav'
+function Home() {
+  return (
+    <LandingNav />
+  )
+}
+
+export default Home

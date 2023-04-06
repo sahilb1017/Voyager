1dd14b51655c6778cab0189f8fc0254a5de97243
Siwon <Siwonkim0206@gmail.com>
2023-04-06 17:24:53 -0600

    lol

--- frontend/src/components/LandingNav.jsx
+++ frontend/src/components/LandingNav.jsx
@@ -29,7 +29,7 @@ export default function LandingNav() {
             FAQ
             <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-main-blue"></span>
           </Link>
-          <Link to="/" class="group transition duration-300 block mt-4 lg:inline-block lg:mt-0 text-main-blue mr-12">
+          <Link to="/Login" class="group transition duration-300 block mt-4 lg:inline-block lg:mt-0 text-main-blue mr-12">
             Login
             <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-main-blue"></span>
           </Link>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "/voyager-logo.png"
+export default function Login() {
+  return (
+    <div class='flex flex-col items-center justify-center'>
+        <div>
+            <img src={Logo} class="fill-current " width="283" height="74" viewBox="0 0 283 74"></img>
+        </div>
+    </div>
+  )
+}
+
